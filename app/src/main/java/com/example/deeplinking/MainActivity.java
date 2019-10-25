a5e2c88f6c46f6296e220319a5826639fe7ffcdd
Vibhu <ananyagautam10@gmail.com>
2019-10-25 18:14:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/deeplinking/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.deeplinking;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        Uri data = intent.getData();
+
+        if(data!=null){
+            TextView text = (TextView)findViewById(R.id.textView);
+            text.setText(data.toString());
+        }
+
+
+    }
+}
